b51b6ff9267b262b465d4d02455729dfe822c972
BFT\yshashkova <julia.rine55@mail.ru>
2025-10-23 15:05:20 +0300

    First commit

---
+++ src/main/java/InvestCalc.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InvestCalc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Введите начальную сумму вклада: ");
+        float currentSum = scanner.nextFloat();
+        System.out.print("Введите количество месяцев: ");
+        int months = scanner.nextInt();
+        float monthlyPercent = 0.07f;
+        for (int i = 1; i <= months; i++) {
+            currentSum = currentSum + (currentSum * monthlyPercent);
+
+            System.out.printf("Конечная сумма вклада через " + months +" месяцев: " + currentSum);
+        }
+    }
+}
